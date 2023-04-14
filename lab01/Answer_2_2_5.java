7d6ff7666cf37d49b7ffa5b34b38bd0634647d29
Dung Le <leducdung203@gmail.com>
2023-04-14 13:44:50 +0700

    Create Answer_2_2_5.java

---
+++ lab01/Answer_2_2_5.java
@@ -0,0 +1,21 @@
+package lab01;
+import.java.util.Scanner;
+public class Answer_2_2_5{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your first number?");
+        double num1 = scanner.nextDouble();
+        System.out.print("What is your second number?");
+        double num2 = scanner.nextDouble();
+        double sum = num1 + num2;
+        double diff = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        scanner.close();
+    }
+    
+}
