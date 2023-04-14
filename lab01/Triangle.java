69dde5d6570524df851019b7c92b88ee6310a7de
Dung Le <leducdung203@gmail.com>
2023-04-14 19:53:30 +0700

    update

---
+++ lab01/Triangle.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the height of the triangle: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+
+            // print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // move to the next line
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
